89e0dbbe18dfde9a0d3069f481b97eaee0ef8ca0
Theo PAYEN <tt.payen@gmail.com>
2022-07-13 19:38:49 +0200

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,9 @@
+apt update && apt full-upgrade -y
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update 
+apt install docker-ce docker-ce-cli containerd.io
