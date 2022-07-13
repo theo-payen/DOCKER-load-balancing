3f8b065da9959130224ef83bde76d8ec23f4b7ed
Theo PAYEN <tt.payen@gmail.com>
2022-07-13 20:57:38 +0200

    update

---
+++ start_docker_WEB_SITE.sh
@@ -0,0 +1,9 @@
+docker run -d -p 8000:8080 -v /DOCKER-load-balancing/WEB_SITE/:/var/www/html/ trafex/php-nginx
+docker run -d -p 8001:8080 -v /DOCKER-load-balancing/WEB_SITE/:/var/www/html/ trafex/php-nginx
+docker run -d -p 8002:8080 -v /DOCKER-load-balancing/WEB_SITE/:/var/www/html/ trafex/php-nginx
+docker run -d -p 8003:8080 -v /DOCKER-load-balancing/WEB_SITE/:/var/www/html/ trafex/php-nginx
+
+#docker run -d -p 8000:80 -v /Docker/Ngnix/1/:/usr/share/nginx/html nginx
+#docker run -d -p 8001:80 -v /Docker/Ngnix/2/:/usr/share/nginx/html nginx
+#docker run -d -p 8002:80 -v /Docker/Ngnix/3/:/usr/share/nginx/html nginx
+#docker run -d -p 80:80 loadbalancingnginx
--- startdocker.sh
+++
@@ -1,9 +0,0 @@
-docker run -d -p 8000:8080 -v /DOCKER-load-balancing/html/:/var/www/html/ trafex/php-nginx
-docker run -d -p 8001:8080 -v /DOCKER-load-balancing/html/:/var/www/html/ trafex/php-nginx
-docker run -d -p 8002:8080 -v /DOCKER-load-balancing/html/:/var/www/html/ trafex/php-nginx
-docker run -d -p 8003:8080 -v /DOCKER-load-balancing/html/:/var/www/html/ trafex/php-nginx
-
-#docker run -d -p 8000:80 -v /Docker/Ngnix/1/:/usr/share/nginx/html nginx
-#docker run -d -p 8001:80 -v /Docker/Ngnix/2/:/usr/share/nginx/html nginx
-#docker run -d -p 8002:80 -v /Docker/Ngnix/3/:/usr/share/nginx/html nginx
-#docker run -d -p 80:80 loadbalancingnginx
