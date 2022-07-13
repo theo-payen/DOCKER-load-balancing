a5b01f653c723f974c0b8dc2f54fbc1de4acc850
Theo PAYEN <tt.payen@gmail.com>
2022-07-14 00:06:51 +0200

    Update boot_strat_docker.sh

--- docker_site/boot_strat_docker.sh
+++ docker_site/boot_strat_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp start_docker_WEB_SITE.sh /etc/init.d/start_docker_WEB_SITE.sh
+cp /DOCKER-load-balancing/docker_site/start_docker_WEB_SITE.sh /etc/init.d/start_docker_WEB_SITE.sh
 chmod 755 /etc/init.d/start_docker_WEB_SITE.sh
